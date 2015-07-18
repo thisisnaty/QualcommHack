30dca94634da314a3be69684aa9be2fae9c8d584
thisisnaty <garcia.doe@gmail.com>
2015-07-18 02:42:26 -0700

    Update script.js
    Template to show the content according to the navigation active button

--- script.js
+++ script.js
@@ -159,3 +159,41 @@ FB.api(
 );
 }
 
+function loadFirst(table){
+  actualContent = table;
+  var appendContent = "";
+  for (var i = 0; i < 10; i++) {
+    appendContent += "<tr>" +
+                        "<td>" +
+                          table +
+                        "</td>" +
+                        "<td>" +
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" + 
+                        "</td>" +
+                      "</tr>";
+  }
+  $('#' + table).append(appendContent);
+  $('#' + table).css("display", "table");
+}
+
+
+
+function loadContent(table) {
+  $('#active').removeAttr('id');
+  $('.circle.' + table + 'Icon').attr('id', 'active');
+  $('#' + actualContent).css({"display": "none"});
+  actualContent = table;
+  var appendContent = "";
+  for (var i = 0; i < 10; i++) {
+    appendContent += "<tr>" +
+                        "<td>" +
+                          table +
+                        "</td>" +
+                        "<td>" +
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" + 
+                        "</td>" +
+                      "</tr>";
+  }
+  $('#' + table).append(appendContent);
+  $('#' + table).css("display", "table");
+}
