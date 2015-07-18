2078fe6d44f6f20e5c8ca5460e47406e85fab244
Kara Vaillancourt <kvail94@vt.edu>
2015-07-18 07:15:32 -0700

    s

--- script.js
+++ script.js
@@ -249,7 +249,7 @@ function grabInternshipPosts(){
 		for (var i = 0; i < postArray.length; i++) {
 		    var data = postArray[i];
 		    if (data == "undefined" > -1) {
-			    if ((data.message).indexOf("internship") > -1) {
+			    if ((data.message).indexOf("internships") > -1) {
 			    	//console.log(data.message); 
 				    //postsHTML += "<tr><div class='fb-post' data-href='" + data.actions[0].link + "' data-width='500px'></div></tr>";
 				    postsHTML += "<tr><td id='postIMG'> <img src='" + data.picture + "'/></td><td id='postMessage'>";
