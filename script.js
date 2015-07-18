5df1af75068dc4ec526995b83aebab159f932b89
Kara Vaillancourt <kvail94@vt.edu>
2015-07-18 06:55:46 -0700

    reduce limit

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ function loadFirst(table){
   var postsHTML ="";
   console.log('you asked for the feed .... '); 
   FB.api(
-    "/AspirationsAward/feed?limit=50",
+    "/AspirationsAward/feed?limit=10",
     function (response) {
       if (response && !response.error) {
       		postArray = response.data; 
