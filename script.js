2973fdccdf88161987db032e0ae78ce9163150db
Kara Vaillancourt <kvail94@vt.edu>
2015-07-18 07:02:07 -0700

    portland

--- script.js
+++ script.js
@@ -248,7 +248,7 @@ function grabInternshipPosts(){
   console.log('you asked for the internship .... '); 
 		for (var i = 0; i < postArray.length; i++) {
 		    var data = postArray[i];
-		    if ((data.message).indexOf("internship") > -1) {
+		    if ((data.message).indexOf("Portland") > -1) {
 		    		console.log(data.message); 
 			    //postsHTML += "<tr><div class='fb-post' data-href='" + data.actions[0].link + "' data-width='500px'></div></tr>";
 			    //postsHTML += "<tr><td id='postIMG'> <img src='" + data.picture + "'/></td><td id='postMessage'>";
