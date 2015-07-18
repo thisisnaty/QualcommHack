0771fc39db44b29d0282107fd00db196ed4130b1
Kara Vaillancourt <kvail94@vt.edu>
2015-07-18 06:54:18 -0700

    (((((( ))))))

--- script.js
+++ script.js
@@ -239,32 +239,6 @@ function loadContent(table) {
 	  $('#' + table).css("display", "table");
 }
 
-function grabPosts(){
-		var postsHTML ="";
-  console.log('you asked for the feed .... '); 
-  FB.api(
-    "/AspirationsAward/feed?limit=50",
-    function (response) {
-      if (response && !response.error) {
-      		postArray = response.data; 
-      		console.log(postArray.length); 
-		for (var i = 0; i < response.data.length; i++) {
-		    var data = response.data[i];
-		    
-		    //postsHTML += "<tr><div class='fb-post' data-href='" + data.actions[0].link + "' data-width='500px'></div></tr>";
-		    postsHTML += "<tr><td id='postIMG'> <img src='" + data.picture + "'/></td><td id='postMessage'>";
-		    postsHTML += "<a class='profileLink' style='text-decoration:none' href='" + data.actions[0].link + "'>" + data.message + " </a></td></tr>"
-		    //console.log(data.message);
-		}
-		$('#posts').append(postsHTML);
-	}
-      else {
-        console.log(response); 
-      }
-    }
-);
-}
-
 function grabPeople(){
 	
 }
@@ -275,7 +249,7 @@ function grabInternshipPosts(){
 		for (var i = 0; i < postArray.length; i++) {
 		    var data = postArray[i];
 		    console.log(data.message); 
-		    if (data.message.search(/internship/i) != -1) {
+		    if ((data.message).search(/internship/i) != -1) {
 			    //postsHTML += "<tr><div class='fb-post' data-href='" + data.actions[0].link + "' data-width='500px'></div></tr>";
 			    postsHTML += "<tr><td id='postIMG'> <img src='" + data.picture + "'/></td><td id='postMessage'>";
 			    postsHTML += "<a class='profileLink' style='text-decoration:none' href='" + data.actions[0].link + "'>" + data.message + " </a></td></tr>"
